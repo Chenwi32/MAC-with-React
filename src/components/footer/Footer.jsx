86ae1d2a922a52109364681ec4ca7188deed7ede
Chenwi <chenwieugene.j@gmail.com>
2022-02-26 13:19:35 +0100

    Worked on the home page layout and what to show on the home page

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -11,7 +11,7 @@ function Footer() {
     <div className="footer ">
       <div className="footer__container container flex__col">
         <div className="footer__main flex">
-          <div className="social flex__col">
+          <div className="social flex">
             <a
               rel="noreferrer"
               target="_blank"
@@ -60,43 +60,6 @@ function Footer() {
               <span className="social__link-name">Twitter</span>
             </a>
           </div>
-
-          <hr />
-
-          <form className="footer__main-form flex__col" action="">
-            <div className="footer__cta__input-container flex__col">
-              <p>
-                If you have any sugestions on how we could improve our System,
-                please leave a message below
-              </p>
-              <label className="footer__cta-label" htmlFor="name">
-                Name:
-              </label>
-              <input type="text" className="footer__cta-input" />
-
-              <label className="footer__cta-label" htmlFor="email">
-                Email:
-              </label>
-              <input type="email" className="footer__cta-input" />
-
-              <label className="footer__cta-label" htmlFor="message">
-                Message:
-              </label>
-
-              <textarea
-                className="message"
-                name="message"
-                id=""
-                cols="30"
-                rows="10"
-              ></textarea>
-            </div>
-            <div className="footer__cta__button flex">
-              <button type="submit" className="btn2">
-                Send
-              </button>
-            </div>
-          </form>
         </div>
 
         <div className="footer__bottom flex">
--- src/components/home/banner/Banner.jsx
+++ src/components/home/banner/Banner.jsx
@@ -1,78 +1,58 @@
 import React from "react";
 import { useHistory } from "react-router";
-import "./Banner.css";
-
-/*//////// Banner Animated Images ///////*/
-
-export const animateText = () => {
-  const feel = document.getElementById("feel");
-  const priority = document.getElementById("priority");
-  const animatedTextCon = document.getElementById("animated__text");
-
-  if (animatedTextCon) {
-    animatedTextCon.innerHTML = feel.innerHTML;
-
-    setInterval(() => {
-      if (animatedTextCon.innerHTML === feel.innerHTML) {
-        animatedTextCon.innerHTML = priority.innerHTML;
-      } else {
-        animatedTextCon.innerHTML = feel.innerHTML;
-      }
-    }, 6000);
-  }
-};
-
-/*//////// End Banner Animated Images ///////*/
+import "./Banner.scss";
 
 function Banner() {
   const history = useHistory();
 
   return (
     <div className="banner-container flex banner">
-      <div className="banner container">
-        <h1 id="backToTop">
-          <span className="m">
-            <em>M</em>
-          </span>
-          <span className="a">A</span>
-          friculture
-        </h1>
+      <div className="banner container flex">
+        <div className="banner-text flex__col">
+          <h1 id="backToTop">
+            <span className="m">
+              <em>M</em>
+            </span>
+            <span className="a">A</span>
+            friculture
+          </h1>
 
-        <div className="quotes">
-          <p>
-            There Is Beauty In Everything — <br />
-            African Fashion Is Never An Exception.
-            <br />
-            <strong>Give The African Style A Try!</strong>
-          </p>
-          <div id="animated__text" className="animated__text"></div>
+          <div className="quotes">
+            <p>
+              There Is Beauty In Everything — <br />
+              African Fashion Is Never An Exception.
+              <br />
+              <strong>Give The African Style A Try!</strong>
+            </p>
+            <div id="animated__text" className="animated__text"></div>
 
-          <p className="feel hide" id="feel">
-            <strong>“FEEL THE ROYALTY”</strong>
-          </p>
-          <p className="hide" id="priority">
-            <strong>
-              <em>Your Satisfaction, Our Priority!</em>
-            </strong>
-          </p>
+            <p className="feel hide" id="feel">
+              <strong>“FEEL THE ROYALTY”</strong>
+            </p>
+            <p className="hide" id="priority">
+              <strong>
+                <em>Your Satisfaction, Our Priority!</em>
+              </strong>
+            </p>
+          </div>
+
+          <div className="buttons flex">
+            <button
+              className="btn banner__btn"
+              onClick={(event) => history.push("/products")}
+            >
+              Shop Now
+            </button>
+            <button className="btn2 banner__btn">Sign Up</button>
+          </div>
         </div>
 
-        <div className="buttons flex">
-          <button
-            className="btn banner__btn"
-            onClick={(event) => history.push("/products")}
-          >
-            Shop Now
-          </button>
-          <button className="btn2 banner__btn">Sign Up</button>
+        <div className="banner-image">
+          <img src="/images/banner2.png" alt="" />
         </div>
       </div>
     </div>
   );
 }
-
-window.onload = () => {
-  animateText();
-};
 
 export default Banner;
